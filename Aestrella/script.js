4e7a1495b81795850aedc611898fd89c01d48d59
Popoliku <andres1239cl@gmail.com>
2024-11-26 23:30:09 +0100

    hihihaha

--- Aestrella/script.js
+++ Aestrella/script.js
@@ -1,4 +1,6 @@
 var draw;
+var route;
+var hihihaha;
 
 document.getElementById("findPathBtn").addEventListener("click", () => {
     document.getElementById("result").innerText = "";
@@ -58,7 +60,7 @@ document.getElementById("findPathBtn").addEventListener("click", () => {
     // jiji(a,b);
     console.log(a.id, b.id);
     const path = Astar(a.id, b.id);
-    const route = [];
+    route = [];
 
     console.log("Camino optimo es: ")
     var camino = "";
@@ -79,12 +81,22 @@ document.getElementById("findPathBtn").addEventListener("click", () => {
     }
     draw = L.polyline(route, {
         color: 'black',
-        weight: 15,
-        snakingSpeed: 150 // Speed of the animation
+        weight: 8,
+        snakingSpeed: 200 // Speed of the animation
     });
 
     // Add the polyline to the map and start the animation
     draw.addTo(map).snakeIn();
+    
+    hihihaha = [];
+    route.forEach((estacion,index)=>{
+        setTimeout(() => {
+            var color = marcas.find(marca=>marca.lat==estacion[0] && marca.long==estacion[1]).color;
+            var icono = createCustomDivIcon(color);
+            const marker = L.marker([estacion[0], estacion[1]], { icon: icono}).addTo(map).bindPopup("Custom Div Icon Marker");
+            hihihaha.push(marker);
+        },200*index);
+    });
 
 
     sleep(route.length*240).then(() => {
@@ -101,7 +113,7 @@ console.log("Script loaded");
 
 
 //MAPA
-const map = L.map('map').setView([-34.6083, -58.38], 13); // Inicializar en Buenos Aires 
+const map = L.map('map').setView([-34.6083, -58.38], 14); // Inicializar en Buenos Aires 
 
 //Anadir el mapa
 
@@ -177,55 +189,6 @@ const estaciones = [
     { "long": -58.3970680747, "lat": -34.6231098658, "id": 34.0, "estacion": "PICHINCHA", "linea": "E" },
 ];
 
-const lineaD = [
-    { "long": -58.373955807, "lat": -34.6078023364, "id": 1.0, "estacion": "CATEDRAL", "linea": "D" },
-    { "long": -58.3805743429, "lat": -34.604245203, "id": 2.0, "estacion": "9 DE JULIO", "linea": "D" },
-    { "long": -58.3851423588, "lat": -34.6015871651, "id": 3.0, "estacion": "TRIBUNALES", "linea": "D" },
-    { "long": -58.393125181, "lat": -34.5996395524, "id": 4.0, "estacion": "CALLAO", "linea": "D" },
-    { "long": -58.3979237556, "lat": -34.5997570808, "id": 5.0, "estacion": "FACULTAD DE MEDICINA", "linea": "D" },
-];
-
-const lineaB = [
-    { "long": -58.3699298501, "lat": -34.6029894966, "id": 6.0, "estacion": "LEANDRO N. ALEM", "linea": "B" },
-    { "long": -58.3750715183, "lat": -34.6032972856, "id": 7.0, "estacion": "FLORIDA", "linea": "B" },
-    { "long": -58.3807148471, "lat": -34.6036371052, "id": 8.0, "estacion": "CARLOS PELLEGRINI", "linea": "B" },
-    { "long": -58.3872961335, "lat": -34.6040935531, "id": 9.0, "estacion": "URUGUAY", "linea": "B" },
-    { "long": -58.3923142351, "lat": -34.6044195429, "id": 10.0, "estacion": "CALLAO", "linea": "B" },
-    { "long": -58.3994742567, "lat": -34.6046429679, "id": 11.0, "estacion": "PASTEUR", "linea": "B" },
-];
-
-const lineaA = [
-    { "long": -58.370968499, "lat": -34.6088103092, "id": 12.0, "estacion": "PLAZA DE MAYO", "linea": "A" },
-    { "long": -58.3742677264, "lat": -34.6085590739, "id": 13.0, "estacion": "PERÚ", "linea": "A" },
-    { "long": -58.3790851531, "lat": -34.6088817212, "id": 14.0, "estacion": "PIEDRAS", "linea": "A" },
-    { "long": -58.382232401, "lat": -34.6090998066, "id": 15.0, "estacion": "LIMA", "linea": "A" },
-    { "long": -58.3867771941, "lat": -34.6094125865, "id": 16.0, "estacion": "SAENZ PE\u00d1A", "linea": "A" },
-    { "long": -58.3926688247, "lat": -34.6092256843, "id": 17.0, "estacion": "CONGRESO", "linea": "A" },
-    { "long": -58.3984269918, "lat": -34.6096459617, "id": 18.0, "estacion": "PASCO", "linea": "A" },
-    { "long": -58.4012075342, "lat": -34.6098335784, "id": 19.0, "estacion": "ALBERTI", "linea": "A" },
-];
-
-const lineaC = [
-    { "long": -58.3740182165, "lat": -34.5911938083, "id": 20.0, "estacion": "RETIRO", "linea": "C" },
-    { "long": -58.377819051, "lat": -34.5950574048, "id": 21.0, "estacion": "SAN MARTIN", "linea": "C" },
-    { "long": -58.3781557828, "lat": -34.601769923, "id": 22.0, "estacion": "LAVALLE", "linea": "C" },
-    { "long": -58.3795299801, "lat": -34.6048437399, "id": 23.0, "estacion": "DIAGONAL NORTE", "linea": "C" },
-    { "long": -58.380610718, "lat": -34.6089833149, "id": 24.0, "estacion": "AV. DE MAYO", "linea": "C" },
-    { "long": -58.3804444697, "lat": -34.6126172798, "id": 25.0, "estacion": "MORENO", "linea": "C" },
-    { "long": -58.3801736105, "lat": -34.6181255993, "id": 26.0, "estacion": "INDEPENDENCIA", "linea": "C" },
-    { "long": -58.3799211789, "lat": -34.6219167322, "id": 27.0, "estacion": "SAN JUAN", "linea": "C" },
-    { "long": -58.3814344339, "lat": -34.6276194523, "id": 28.0, "estacion": "CONSTITUCION", "linea": "C" },
-];
-
-const lineaE = [
-    { "long": -58.3736842242, "lat": -34.6092424289, "id": 29.0, "estacion": "BOLIVAR", "linea": "E" },
-    { "long": -58.3775808865, "lat": -34.6128491058, "id": 30.0, "estacion": "BELGRANO", "linea": "E" },
-    { "long": -58.3815349417, "lat": -34.617937394, "id": 31.0, "estacion": "INDEPENDENCIA", "linea": "E" },
-    { "long": -58.3851485496, "lat": -34.6223394919, "id": 32.0, "estacion": "SAN JOSE", "linea": "E" },
-    { "long": -58.3915117, "lat": -34.6227196661, "id": 33.0, "estacion": "ENTRE RÍOS", "linea": "E" },
-    { "long": -58.3970680747, "lat": -34.6231098658, "id": 34.0, "estacion": "PICHINCHA", "linea": "E" },
-];
-
 const graph = new Map();
 estaciones.forEach(estacion => {
     let adyacentes = estaciones.filter(station => (estacion.linea == station.linea) &&
@@ -256,62 +219,110 @@ console.log(graph)
 
 
 //Circulo rojo para estacion
+
+function createCustomDivIcon(color) { 
+    return L.divIcon({ 
+        className: 'custom-div-icon', 
+        html: `<div style="background-color: #000000; width: 20px; height: 20px; border: 4px solid ${color}; border-radius: 50%;"></div>`, 
+        iconSize: [20, 20] 
+    }); 
+}
+
 const customDivIconD = L.divIcon({
     className: 'custom-div-icon',
-    html: "<div style=' background-color: white; width: 20px; height: 20px; border: 4px solid green; border-radius: 50%; '></div>",
+    html: "<div style=' background-color: #f1f1f1; width: 20px; height: 20px; border: 4px solid green; border-radius: 50%; '></div>",
     iconSize: [20, 20]
 });
 
 const customDivIconB = L.divIcon({
     className: 'custom-div-icon',
-    html: "<div style='background-color: red; width: 20px; height: 20px; border-radius: 50%;'></div>",
+    html: "<div style='background-color: #f1f1f1; width: 20px; height: 20px; border: 4px solid red; border-radius: 50%;'></div>",
     iconSize: [20, 20]
 });
 
 const customDivIconA = L.divIcon({
     className: 'custom-div-icon',
-    html: "<div style='background-color: lightblue; width: 20px; height: 20px; border-radius: 50%;'></div>",
+    html: "<div style='background-color: #f1f1f1; width: 20px; height: 20px;border: 4px solid lightblue; border-radius: 50%;'></div>",
     iconSize: [20, 20]
 });
 
 const customDivIconC = L.divIcon({
     className: 'custom-div-icon',
-    html: "<div style='background-color: blue; width: 20px; height: 20px; border-radius: 50%;'></div>",
+    html: "<div style='background-color: #f1f1f1; width: 20px; height: 20px; border: 4px solid blue; border-radius: 50%;'></div>",
     iconSize: [20, 20]
 });
 
 const customDivIconE = L.divIcon({
     className: 'custom-div-icon',
-    html: "<div style='background-color: purple; width: 20px; height: 20px; border-radius: 50%;'></div>",
+    html: "<div style='background-color: #f1f1f1; width: 20px; height: 20px;border: 4px solid purple; border-radius: 50%;'></div>",
     iconSize: [20, 20]
 });
 
+const marcas = estaciones.map(estacion=>{
+    var icono;
+    var color;
+    if(estacion.linea=="D") {
+        icono = customDivIconD;
+        color = "green";
+    }
+    if(estacion.linea=="C") {
+        icono = customDivIconC;
+        color = "blue";
+    }
+    if(estacion.linea=="A") {
+        icono = customDivIconA;
+        color = "lightblue";
+    }
+    if(estacion.linea=="E") {
+        icono = customDivIconE;
+        color = "purple";
+    }
+    if(estacion.linea=="B") {
+        icono = customDivIconB;
+        color = "red";
+    }
+    let atributos = {
+        "long": estacion.long,
+        "lat": estacion.lat,
+        "id": estacion.id,
+        "estacion": estacion.estacion,
+        "linea": estacion.linea,
+        "marker": L.marker([estacion.lat, estacion.long], { icon: icono }).addTo(map).bindPopup("Custom Div Icon Marker"),
+        "color": color
+    }
+    return atributos;
+});
+
+const lineaD = marcas.filter(estacion=>estacion.linea=="D");
+
+const lineaB = marcas.filter(estacion=>estacion.linea=="B");
+
+const lineaA = marcas.filter(estacion=>estacion.linea=="A");
+
+const lineaC = marcas.filter(estacion=>estacion.linea=="C");
+
+const lineaE = marcas.filter(estacion=>estacion.linea=="E");
 
 
 lineaD.forEach(estacion => {
-    const marker = L.marker([estacion.lat, estacion.long], { icon: customDivIconD }).addTo(map).bindPopup("Custom Div Icon Marker");
-    marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
+    estacion.marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
 });
 
 lineaB.forEach(estacion => {
-    const marker = L.marker([estacion.lat, estacion.long], { icon: customDivIconB }).addTo(map).bindPopup("Custom Div Icon Marker");
-    marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
+    estacion.marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
 
 });
 
 lineaA.forEach(estacion => {
-    const marker = L.marker([estacion.lat, estacion.long], { icon: customDivIconA }).addTo(map).bindPopup("Custom Div Icon Marker");
-    marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
+    estacion.marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
 });
 
 lineaC.forEach(estacion => {
-    const marker = L.marker([estacion.lat, estacion.long], { icon: customDivIconC }).addTo(map).bindPopup("Custom Div Icon Marker");
-    marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
+    estacion.marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
 });
 
 lineaE.forEach(estacion => {
-    const marker = L.marker([estacion.lat, estacion.long], { icon: customDivIconE }).addTo(map).bindPopup("Custom Div Icon Marker");
-    marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
+    estacion.marker.bindPopup(`<b>${estacion.estacion}</b><br>Linea: ${estacion.linea}`); //saber que linea es la parada    
 });
 
 
@@ -332,6 +343,9 @@ function handler(inputField, markerGroup) {
     if(draw){
         map.removeLayer(draw);
     }
+
+    if(hihihaha) hihihaha.forEach(marca=>map.removeLayer(marca));
+
     console.log(`Input changed for: ${inputField.id}`);
 
     // Clear markers only for this input
@@ -402,7 +416,7 @@ const routeD = [
     [estaciones[4].lat, estaciones[4].long],
 ];
 
-L.polyline(routeD, { color: 'green', weight: 5 }).addTo(map);
+L.polyline(routeD, { color: 'green', weight: 8 }).addTo(map);
 
 const routeB = [
     [estaciones[5].lat, estaciones[5].long],
